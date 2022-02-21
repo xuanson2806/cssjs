af0cfcd11f9e6e6880dd9cb9c0fcb096ef5d0be8
xuanson2806 <100117834+xuanson2806@users.noreply.github.com>
2022-02-21 15:05:43 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://nhaphokhangdien.net'>http://nhaphokhangdien.net"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://nhaphokhangdien.net'>http://nhaphokhangdien.net</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
